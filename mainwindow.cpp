0e18be0f86859d15f8b808543bff9d4b66d54762
Alexey <alexeydruz24@gmail.com>
2024-10-25 00:41:00 +0700

    change mainwindow2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "tabledialog.h"
+#include "findreplacedialog.h"
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -59,6 +61,142 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+
+
+
+    ui->textEdit->clear();
+
+    currentFile.clear();
+
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+
+
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+
+    currentFile = fileName;
+    QTextStream in(&file);
+
+    QString content = in.readAll();
+    ui->textEdit->setHtml(content);
+
+    file.close();
+}
+
+void MainWindow::saveToTempFile()
+{
+    if(!tempFile.isOpen()){
+        if(!tempFile.open()){
+            QMessageBox::warning(this, "Ошибка", "Не удалось открыть временный файл для записи.");
+            return;
+        }
+    }
+
+    QTextStream out(&tempFile);
+    out << ui->textEdit->toHtml();
+    tempFile.flush();
+    tempFile.close();
+}
+
+void MainWindow::restoreFromTempFile()
+{
+    if(tempFile.isOpen()){
+        tempFile.close();
+    }
+
+    if(!tempFile.open()){
+        QMessageBox::warning(this, "Ошибка", "Не удалось открыть временный файл для записи.");
+        return;
+    }
+
+    QTextStream in(&tempFile);
+    QString content = in.readAll();
+    ui->textEdit->setHtml(content);
+    tempFile.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    if(currentFile.isEmpty()){
+        QString fileName = QFileDialog::getSaveFileName(this, "Файл сохранился");
+        if(fileName.isEmpty()){
+            return;
+        }
+        currentFile = fileName;
+    }
+
+    QFile file(currentFile);
+
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Ошибка", "Файл не сохранился: " + file.errorString());
+        return;
+    }
+
+    QTextStream out(&file);
+    QString content = ui->textEdit->toHtml();
+    out << content;
+    file.close();
+}
+
+void MainWindow::on_actionClear_triggered()
+{
+
+    saveToTempFile();
+    ui->textEdit->clear();
+
+
+}
+
+void MainWindow::on_actionRestore_triggered()
+{
+    restoreFromTempFile();
+}
+
+
+
+void MainWindow::on_addTableButton_clicked()
+{
+
+    TableDialog *dialog = new TableDialog(this);
+    connect(dialog, &TableDialog::accepted, this, &MainWindow::on_tableDialogAccepted);
+
+    dialog->exec();
+}
+
+
+void MainWindow::on_tableDialogAccepted()
+{
+    TableDialog *dialog = qobject_cast<TableDialog *>(sender());
+    if (dialog) {
+        int rows = dialog->getRowCount();
+        int columns = dialog->getColumnCount();
+
+
+        QString tableHtml = "<table border='1'>";
+        for (int i = 0; i < rows; ++i) {
+            tableHtml += "<tr>";
+            for (int j = 0; j < columns; ++j) {
+                tableHtml += "<td style='padding: 20px;'> </td>";
+            }
+            tableHtml += "</tr>";
+        }
+        tableHtml += "</table>";
+
+        ui->textEdit->insertHtml(tableHtml);
+    }
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,29 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QTextStream>
 #include <QMainWindow>
+#include <QTextEdit>
+#include <QTableWidget>
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTemporaryFile>
+#include <QFontDialog>
+#include <QColorDialog>
+#include <QComboBox>
+#include <QFontDatabase>
+#include <QSpinBox>
+#include <QTextTable>
+#include <QTextTableCell>
+#include <QTextCharFormat>
+#include <QTextCursor>
+#include <QLabel>
+#include <QPushButton>
+#include <QInputDialog>
+#include <QSettings>
+#include <QFontComboBox>
+#include <QToolBar>
 
 
 
@@ -19,43 +41,46 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
-};
-
-class EditorWindow : public QMainWindow {
-    Q_OBJECT
-
-public:
-    EditorWindow(QWidget *parent = nullptr);
-    ~EditorWindow();
 
 private slots:
-    void newFile();
-    void openFile();
-    void saveFile();
-    void searchInText();
-    void replaceText();
-    void clearContent();
-    void restoreContent();
-    void changeFont();
-    void setTextColorScheme();
-    void setTableCellPadding();
+    void on_actionNew_triggered();
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionClear_triggered();
+    void on_addTableButton_clicked();
+    void on_tableDialogAccepted();
 
-protected:
-    void closeEvent(QCloseEvent *event) override;
+    void onFontChanged(const QFont &font);
+    void onFontSizeChanged(int size);
+    void onTextColorChanged();
+    void onPaddingChanged(int padding);
+    void onCellColorChanged();
+
+    void on_actionFind_triggered();
+    void findNext();
+    void replace();
+    void replaceAll();
+
+
+    void saveToTempFile();
+    void restoreFromTempFile();
+    void on_actionRestore_triggered();
+
+    void closeEvent(QCloseEvent *event);
+
+    void saveSettings();
+    void loadSettings();
+
+
 
 private:
-    QTextEdit *textEditor;
-    QTableWidget *tableEditor;
-    QString currentFilePath;
+    Ui::MainWindow *ui;
+    QString currentFile;
     QTemporaryFile tempFile;
-
-    void createActions();
-    void createMenus();
-    void createToolBars();
-    void createStatusBar();
+    int currentPadding = 10;
+    QColor currentCellColor;
+    FindReplaceDialog *findReplaceDialog;
+    void mergeFormatOnWordOrSelection(const QTextCharFormat &format);
 };
-
 
 #endif // MAINWINDOW_H
